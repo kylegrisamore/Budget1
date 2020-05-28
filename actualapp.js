6c093d2b3d2d000d65d36ce42edc60055bc03388
kylegrisamore <65174925+kylegrisamore@users.noreply.github.com>
2020-05-28 12:26:23 -0400

    Update actualapp.js

--- actualapp.js
+++ actualapp.js
@@ -34,3 +34,40 @@ var budgetController = (function() {
                 });
                 data.totals[type] = sum;
             };
+
+            var data = {
+                allItems: {
+                    exp: [],
+                    inc: []
+                },
+                totals: {
+                    exp: 0,
+                    inc: 0
+                },
+                budget: 0,
+                percentage: -1
+            };
+
+
+            return {
+                addItem: function(type, des, val) {
+                    var newItem, ID;
+
+                    // Create new ID
+                    if (data.allItems[type].length > 0) {
+                        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+                    } else {
+                        ID = 0;
+                    }
+
+                    if (type === 'exp') {
+                        newItem = new Expense(ID, des, val);
+                    } else if (type === 'inc') {
+                        newItem = new Income(ID, des, val);
+                    }
+
+                    data.allItems[type].push(newItem);
+
+                    // Return the new element
+                    return newItem;
+                },
