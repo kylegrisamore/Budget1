25279d60d6750cf325ba37c2bd3b2b0d94316a1d
kylegrisamore <65174925+kylegrisamore@users.noreply.github.com>
2020-05-28 12:08:16 -0400

    Update actualapp.js

--- actualapp.js
+++ actualapp.js
@@ -6,3 +6,11 @@ var budgetController = (function() {
                 this.value = value;
                 this.percentage = -1;
             };
+
+            Expense.prototype.calcPercentage = function(totalIncome) {
+                if (totalIncome > 0) {
+                    this.percentage = Math.round((this.value / totalIncome) * 100);
+                } else {
+                    this.percentage = -1;
+                }
+            };
